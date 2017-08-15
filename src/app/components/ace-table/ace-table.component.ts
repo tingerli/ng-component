4b4571b1a4129e5636120e50aba2d47e3840c47c
linzhiwei <975020089@qq.com>
2017-08-15 18:03:06 +0800

    下一步做  数据传入  表头数据传入  事件返回

--- src/app/components/ace-table/ace-table.component.ts
+++ src/app/components/ace-table/ace-table.component.ts
@@ -7,11 +7,20 @@ import { Component, OnInit, ViewEncapsulation, Input, Output} from '@angular/cor
   encapsulation: ViewEncapsulation.None
 })
 export class AceTableComponent implements OnInit {
+  @Input() title:string;      //表格标题
+  @Input() loading:boolean;   //加载中
+  @Input() dataSource:Array<object> //遍历的数据
+  @Input() theadSource:Array<object> //表头数据
+  @Input() totalNum:number;    //数据总数
+  @Input() currPage:number;    //当前页码
+  @Input() selectMode:string;  //单选还是多选
+
   private closeStatus:boolean = false; //收起
   constructor() { }
 
   ngOnInit() {
   }
+  
   // 收起表单
   close(){
     this.closeStatus = !this.closeStatus;
