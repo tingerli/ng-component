859d53dffc0d3e87138959f4afe15d1f2a1b7993
linzhiwei <975020089@qq.com>
2017-08-22 18:13:40 +0800

    ace-modal 组件初步完成，正在编写文档

--- src/app/components/ace-modal/ace-modal.component.ts
+++ src/app/components/ace-modal/ace-modal.component.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit, ViewEncapsulation, animate, transition,Input, trigger,state,style} from '@angular/core';
-
+import { Component, OnInit,ViewEncapsulation,Input, Output, EventEmitter} from '@angular/core';
+import { trigger,state,style,transition,animate,keyframes } from '@angular/animations';
+interface ModalSetting {
+  show:boolean;  //状态
+  title:string;  //标题
+  enSureBtnText?:string; //确定按钮的文字
+  cancelBtnText?:string; //取消按钮的文字
+}
 
 @Component({
   selector: 'ace-modal',
@@ -8,41 +14,47 @@ import { Component, OnInit, ViewEncapsulation, animate, transition,Input, trigge
   encapsulation: ViewEncapsulation.None,
   animations: [ // 动画的内容
     trigger('slide', [
-      state('shown' , style({  opacity: 1,  transform: 'translateY(0)'})),
-      state('hidden', style({  opacity: 0,  transform: 'translateY(-40px)'})),
+      state('shown' , style({visibility:"visible",  opacity: 1,  transform: 'translateY(0)'})),
+      state('hidden', style({visibility:"hidden",opacity: 0,  transform: 'translateY(-40px)'})),
       transition('shown => hidden', animate('150ms')),
-      transition('hidden => shown', animate('300ms')),
+      transition('hidden => shown', animate('200ms')),
     ]),
     trigger('maskAnimate', [
-      // state 控制不同的状态下对应的不同的样式
-      state('shown' , style({ opacity: 1})),
-      state('hidden', style({ opacity: 0})),
-      transition('shown => hidden', animate('100ms')),
-      transition('hidden => shown', animate('300ms')),
+      state('shown' , style({visibility:"visible", opacity: 1})),
+      state('hidden', style({visibility:"hidden", opacity: 0})),
+      transition('shown => hidden', animate('125ms')),
+      transition('hidden => shown', animate('200ms')),
     ])
   ]
 })
 export class AceModalComponent implements OnInit {
-  private eleState:string = 'hidden';
-  private maskClass:string;
-  private modalClass:string;
-  constructor() { }
+  @Input() state:ModalSetting = {           //控制状态值
+    show:false,
+    title:'消息框'
+  };
+  private get animate(){           //动画状态
+    return this.state.show?"shown":"hidden"
+  };
+  private set animate(val){
+    
+  };
+
+  @Output() onSure = new EventEmitter();  //点击确定按钮
+  
+  constructor(
+  ) { }
 
   ngOnInit() {
-
+    
   }
-  
+
   //关闭
   onClose(){
-    this.maskClass = "ace-modal-hidden";
-    this.openModal();
+    this.state.show = false;
   }
   
-  openModal(){
-    if(this.eleState=='shown'){
-      this.eleState = 'hidden';
-    }else{
-      this.eleState = 'shown';
-    }
+  //确定按钮
+  onClickSureBtn(){
+    this.onSure.emit();
   }
 }
--- src/app/modal-page/modal-page.component.ts
+++ src/app/modal-page/modal-page.component.ts
@@ -7,10 +7,55 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class ModalPageComponent implements OnInit {
+  private modalState={
+    show:false
+  };
 
   constructor() { }
 
   ngOnInit() {
   }
+  
+  openModal(){
+    this.modalState.show = true;
+  }
 
+  thead = ['属性','说明','类型',"接口",'默认值'];
+  tableValue=[
+    {
+      name:'state',
+      text:'设置modal框的状态',
+      type:'object',
+      default:`
+      {  <br>
+        &nbsp;&nbsp;show:false,<br>
+        &nbsp;&nbsp;title:'消息框'<br>
+      }`,
+      iterface:`interface ModalSetting {<br>
+        &nbsp;&nbsp;show:boolean;  &nbsp;&nbsp;//状态<br>
+        &nbsp;&nbsp;title:string;  &nbsp;&nbsp;//标题<br>
+        &nbsp;&nbsp;enSureBtnText?:string; &nbsp;&nbsp;//确定按钮的文字<br>
+        &nbsp;&nbsp;cancelBtnText?:string; &nbsp;&nbsp;//取消按钮的文字<br>
+      }`
+    },
+  ];
+  eventHeader = ['事件','说明',"返回值"];
+  eventValue = [
+    {
+      name:'onSure',
+      text:"点击确定按钮后触发",
+      cb:"无"
+    }
+  ];
+  getKeys(item:any){
+    return [item.name, item.text, item.type,item.iterface,item.default]
+  }
+
+  getEventKeys(item:any) {
+    return [item.name, item.text, item.cb]
+  }
+  
+  onclick(val) {
+    console.log(val);
+  }
 }
